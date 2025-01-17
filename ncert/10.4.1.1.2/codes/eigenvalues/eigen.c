abb1555dfd0047074df4df6fbe8239080bc1736f
Dwarak-A <you@example.com>
2025-01-18 00:32:58 +0530

    Add existing files

--- ncert/10.4.1.1.2/codes/eigenvalues/eigen.c
+++ ncert/10.4.1.1.2/codes/eigenvalues/eigen.c
@@ -4,6 +4,60 @@
 #include <math.h>
 #include "functions.h"
 
+double complex *eigenvalues(double complex **A, int dim) {
+    double complex **H = makehessberg(A, dim);
+    double complex sigma;
+    double tolerance = 1e-10;
+
+    for (int m = dim; m > 1; m--) {
+        int iterations = 0;
+
+        while (iterations < 20*dim) {
+            iterations++;
+            sigma = H[m-1][m-1];
+            double complex **sigmaI = Matscale(identity(dim), dim, dim, sigma);
+            double complex **H_shifted = Matsub(H, sigmaI, dim, dim);
+
+            double complex **Q_T = identity(dim);
+
+            for (int p = 1; p < m; p++) {
+                double complex xi = H_shifted[p-1][p-1];
+                double complex xj = H_shifted[p][p-1];
+                if (cabs(xj) < tolerance) {
+                    continue;
+                }
+
+                double complex c = conj(xi)/(csqrt(cabs(xi)*cabs(xi)+cabs(xj)*cabs(xj)));
+		double complex s = conj(xj)/(csqrt(cabs(xi)*cabs(xi)+cabs(xj)*cabs(xj)));
+		
+                double complex **Gi = identity(dim);
+                Gi[p-1][p-1] = c;
+                Gi[p-1][p] = s;
+                Gi[p][p-1] = -conj(s);
+                Gi[p][p] = conj(c);
+                H_shifted = Matmul(Gi, H_shifted, dim, dim, dim);
+                Q_T = Matmul(Q_T, transposeMat(Gi, dim, dim), dim, dim, dim);
+
+                freeMat(Gi, dim);
+            }
+            H = Matmul(H_shifted, Q_T, dim, dim, dim);
+            H = Matadd(H, sigmaI, dim, dim);
+            freeMat(sigmaI, dim);
+            freeMat(H_shifted, dim);
+            freeMat(Q_T, dim);
+
+                if (cabs(H[m-1][m-2]) > tolerance) {
+                    continue;
+                }
+                else{
+                	break;
+                }
+        }
+    }
+
+    return calcuppereig(H,dim);
+}
+
 // Function to solve a polynomial of a given degree and return its roots
 double complex *solve_polynomial(int degree, double complex A[degree+1]) {
     // Check if the leading coefficient is zero
